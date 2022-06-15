d8bf6e1b1b2d01d4a240ec5714de48b42fb4231e
vitaliiPsl <vitalii.psl.main@gmail.com>
2022-06-15 12:22:51 +0300

    Front end: add context and display api errors

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import '../header/Header.css';
 import {withRouter} from "../../WithRouter";
 import Header from "../header/Header";
+import authService from "../../services/auth.service";
+import {MainContext} from "./MainContext";
+import ErrorsBox from "../errors/ErrorsBox";
 
 class App extends React.Component {
     constructor(props) {
@@ -13,6 +16,8 @@ class App extends React.Component {
             user: user,
             setUser: this.setUser,
             removeUser: this.removeUser,
+            errors: [],
+            handleError: this.handleApiError,
         }
     }
 
@@ -24,11 +29,40 @@ class App extends React.Component {
         this.setState({user: null});
     }
 
+    handleApiError = async (response) => {
+        let apiError = await response.json();
+
+        if (apiError.subErrors.length === 0) {
+            this.addError(apiError.message)
+        } else {
+            for (let subError of apiError.subErrors) {
+                this.addError(subError.message);
+            }
+        }
+    }
+
+    addError = (error) => {
+        let errors = this.state.errors;
+        errors.push(error);
+
+        this.setState({errors: errors});
+    }
+
+    removeError = (index) => {
+        let errors = this.state.errors;
+        errors.splice(index, 1);
+
+        this.setState({errors: errors});
+    }
+
     render() {
         return (
-            <div className="App">
-                <Header/>
-            </div>
+            <MainContext.Provider value={this.state}>
+                <div className="App">
+                    <Header/>
+                    <ErrorsBox errors={this.state.errors} removeError={this.removeError}/>
+                </div>
+            </MainContext.Provider>
         );
     }
 }
