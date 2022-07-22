88c7ff924a34e0c9a7fd13c89628dc9d0cd5ee12
Vitalii Psl <vitalii.psl.main@gmail.com>
2022-07-22 13:16:29 +0300

    Remove unused validation constraint from User entity and add @JsonIgnore to password field

--- src/main/java/com/example/cinema/model/entities/User.java
+++ src/main/java/com/example/cinema/model/entities/User.java
@@ -1,14 +1,12 @@
 package com.example.cinema.model.entities;
 
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import javax.validation.constraints.*;
 import java.util.Objects;
 
-@Data @NoArgsConstructor @AllArgsConstructor
+@Data
 @Entity
 @Table(name = "person")
 public class User {
@@ -16,19 +14,13 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @NotBlank(message = "You need to provide your first name")
     private String firstName;
-
-    @NotBlank(message = "You need to provide your last name")
     private String lastName;
 
     @Column(unique = true)
-    @NotBlank(message = "You have to provide your email")
-    @Email(message = "Your need to provide correct email address")
     private String email;
 
-    @NotBlank
-    @Size(min = 6, message = "Your password must contain at least 6 symbols")
+    @JsonIgnore
     private String password;
 
     @Enumerated(EnumType.STRING)
