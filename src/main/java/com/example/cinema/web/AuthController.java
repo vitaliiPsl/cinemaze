f8441c7e058c0d80420d63ee4cfa3b759ef306bf
Vitalii Psl <vitalii.psl.main@gmail.com>
2022-07-22 19:49:51 +0300

    Move User - UserDto mapping from service to controller

--- src/main/java/com/example/cinema/web/AuthController.java
+++ src/main/java/com/example/cinema/web/AuthController.java
@@ -3,13 +3,18 @@ package com.example.cinema.web;
 import com.example.cinema.config.security.jwt.JwtUtils;
 import com.example.cinema.model.dto.AuthenticationRequestDto;
 import com.example.cinema.model.dto.UserDto;
+import com.example.cinema.model.entities.User;
 import com.example.cinema.service.UserService;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.Map;
@@ -18,16 +23,20 @@ import java.util.Map;
 @RequestMapping("/api/auth")
 @AllArgsConstructor
 public class AuthController {
+    private final ModelMapper modelMapper;
     private final UserService userService;
     private final AuthenticationManager authenticationManager;
 
     @PostMapping("/signup")
-    public UserDto signup(@Valid @RequestBody UserDto user){
-        return userService.saveClient(user);
+    public UserDto signup(@Valid @RequestBody UserDto userDto) {
+        User user = modelMapper.map(userDto, User.class);
+        user = userService.saveClient(user);
+
+        return modelMapper.map(user, UserDto.class);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<Map<String, Object>> login(@RequestBody AuthenticationRequestDto request) {
+    public ResponseEntity<Map<String, Object>> login(@Valid @RequestBody AuthenticationRequestDto request) {
         // Authenticate user. It will throw AuthenticationExc if credentials are invalid
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword());
         Authentication authentication = authenticationManager.authenticate(authenticationToken);
@@ -37,7 +46,9 @@ public class AuthController {
 
         // Find user by email
         String username = authentication.getName();
-        UserDto userDto = userService.getUser(username);
+        User user = userService.getUser(username);
+
+        UserDto userDto = modelMapper.map(user, UserDto.class);
 
         // return response entity containing user and authentication token
         Map<String, Object> response = Map.of("user", userDto, "jwt", token);
--- src/test/java/com/example/cinema/service/UserServiceTest.java
+++ src/test/java/com/example/cinema/service/UserServiceTest.java
@@ -2,36 +2,31 @@ package com.example.cinema.service;
 
 import com.example.cinema.exceptions.EntityAlreadyExistsException;
 import com.example.cinema.exceptions.EntityNotFoundException;
-import com.example.cinema.model.dto.UserDto;
 import com.example.cinema.model.entities.Role;
 import com.example.cinema.model.entities.User;
 import com.example.cinema.persistence.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
-    @Mock
-    PasswordEncoder passwordEncoder;
 
     @Mock
-    ModelMapper modelMapper;
+    PasswordEncoder passwordEncoder;
 
     @Mock
     UserRepository userRepository;
@@ -46,45 +41,42 @@ class UserServiceTest {
         String password = "password";
         String encodedPassword = "encoded";
 
-        User user = getUser(0, email);
-        UserDto userDto = getUserDto(email);
-        userDto.setPassword(password);
+        User user = getUser(0, email, password);
 
         // when
-        when(userRepository.findByEmail(email)).thenReturn(Optional.empty());
         when(passwordEncoder.encode(password)).thenReturn(encodedPassword);
-        when(modelMapper.map(userDto, User.class)).thenReturn(user);
-        when(modelMapper.map(user, UserDto.class)).thenReturn(getUserDto(email));
-        UserDto result = userService.saveClient(userDto);
+        when(userRepository.findByEmail(email)).thenReturn(Optional.empty());
+        when(userRepository.save(user)).thenReturn(user);
+
+        User result = userService.saveClient(user);
 
         // then
-        assertThat(userDto.getPassword(), equalTo(null));
-        assertThat(user.getPassword(), equalTo(encodedPassword));
-        assertThat(user.getRole(), equalTo(Role.CLIENT));
-        assertThat(result.getEmail(), equalTo(email));
         verify(passwordEncoder).encode(anyString());
         verify(userRepository).save(user);
+
+        assertThat(result.getPassword(), equalTo(encodedPassword));
+        assertThat(result.getRole(), equalTo(Role.CLIENT));
+        assertThat(result.getEmail(), equalTo(email));
     }
 
     @Test
     void testRegisterClientThrowsExceptionIfUserAlreadyExists() {
         // given
         String email = "test@mail.com";
-        UserDto userDto = getUserDto(email);
-        User user = getUser(0, email);
+        User user = getUser(0, email, "");
 
         // when
         when(userRepository.findByEmail(email)).thenReturn(Optional.of(user));
 
         // then
-        assertThrows(EntityAlreadyExistsException.class, () -> userService.saveClient(userDto));
+        assertThrows(EntityAlreadyExistsException.class, () -> userService.saveClient(user));
     }
 
     @Test
     void testDeleteUser() {
         // given
         long id = 1;
-        User user = getUser(id, "");
+        User user = getUser(id, "", "");
 
         // when
         when(userRepository.findById(id)).thenReturn(Optional.of(user));
@@ -112,18 +104,15 @@ class UserServiceTest {
         // given
         long id = 1;
         String email = "test@mail.com";
-        User user = getUser(id, email);
-        UserDto userDto = getUserDto(user.getEmail());
+        User user = getUser(id, email, "");
 
         // when
         when(userRepository.findById(id)).thenReturn(Optional.of(user));
-        when(modelMapper.map(user, UserDto.class)).thenReturn(userDto);
-        UserDto result = userService.getUser(id);
+        User result = userService.getUser(id);
 
         // then
         verify(userRepository).findById(id);
-        verify(modelMapper).map(user, UserDto.class);
-        assertThat(result.getEmail(), equalTo(userDto.getEmail()));
+        assertThat(result.getEmail(), is(email));
     }
 
     @Test
@@ -144,18 +133,15 @@ class UserServiceTest {
         // given
         long id = 1;
         String email = "test@mail.com";
-        User user = getUser(id, email);
-        UserDto userDto = getUserDto(user.getEmail());
+        User user = getUser(id, email, "");
 
         // when
         when(userRepository.findByEmail(email)).thenReturn(Optional.of(user));
-        when(modelMapper.map(user, UserDto.class)).thenReturn(userDto);
-        UserDto result = userService.getUser(email);
+        User result = userService.getUser(email);
 
         // then
         verify(userRepository).findByEmail(email);
-        verify(modelMapper).map(user, UserDto.class);
-        assertThat(result.getEmail(), equalTo(userDto.getEmail()));
+        assertThat(result.getEmail(), equalTo(user.getEmail()));
     }
 
     @Test
@@ -174,49 +160,26 @@ class UserServiceTest {
     void testGetAllUsers() {
         // given
         List<String> emails = List.of("test@mail.com", "test1@email.com", "test2@email.com");
-        List<User> users = List.of(getUser(0, "test@mail.com"), getUser(0, "test1@mail.com"), getUser(0, "test2@mail.com"));
+        List<User> users = List.of(
+                getUser(0, "test@mail.com", ""),
+                getUser(0, "test1@mail.com", ""),
+                getUser(0, "test2@mail.com", "")
+        );
 
         // when
         when(userRepository.findAll()).thenReturn(users);
-        for (var user : users) {
-            when(modelMapper.map(user, UserDto.class)).thenReturn(getUserDto(user.getEmail()));
-        }
-        List<UserDto> resultDtoList = userService.getAllUsers();
+        List<User> resultList = userService.getAllUsers();
 
         // then
         verify(userRepository).findAll();
-        verify(modelMapper, times(users.size())).map(ArgumentMatchers.any(User.class), eq(UserDto.class));
-        assertThat(resultDtoList, hasSize(emails.size()));
+        assertThat(resultList, hasSize(emails.size()));
     }
 
-    @Test
-    void testMapUserToUserDto() {
-        // given
-        String email = "test@mail.com";
-        User user = getUser(0, email);
-        UserDto userDto = getUserDto(email);
-
-        // when
-        when(modelMapper.map(user, UserDto.class)).thenReturn(userDto);
-        UserDto result = userService.mapUserToUserDto(user);
-
-        // then
-        verify(modelMapper).map(user, UserDto.class);
-        assertThat(result.getEmail(), equalTo(userDto.getEmail()));
-    }
-
-    private UserDto getUserDto(String email) {
-        UserDto userDto = new UserDto();
-        userDto.setEmail(email);
-
-        return userDto;
-    }
-
-    private User getUser(long id, String email) {
-        User user = new User();
-        user.setId(id);
-        user.setEmail(email);
-
-        return user;
+    private User getUser(long id, String email, String password) {
+        return User.builder()
+                .id(id)
+                .email(email)
+                .password(password)
+                .build();
     }
 }
