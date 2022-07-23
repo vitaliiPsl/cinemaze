068641db2f6f646f01aca38d4def90ceb7492ebd
Vitalii Psl <vitalii.psl.main@gmail.com>
2022-07-23 12:13:51 +0300

    Move MovieSession - MovieSessionDto mapping from service to controller

--- src/main/java/com/example/cinema/service/MovieSessionService.java
+++ src/main/java/com/example/cinema/service/MovieSessionService.java
@@ -1,7 +1,6 @@
 package com.example.cinema.service;
 
 import com.example.cinema.exceptions.EntityNotFoundException;
-import com.example.cinema.model.dto.MovieSessionDto;
 import com.example.cinema.model.entities.movie.Movie;
 import com.example.cinema.model.entities.movie.MovieHall;
 import com.example.cinema.model.entities.movie.MovieSession;
@@ -10,128 +9,103 @@ import com.example.cinema.persistence.MovieRepository;
 import com.example.cinema.persistence.MovieSessionRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @AllArgsConstructor
 @Service
 @Transactional
 public class MovieSessionService {
-    private ModelMapper modelMapper;
     private MovieSessionRepository movieSessionRepository;
     private MovieRepository movieRepository;
     private MovieHallRepository movieHallRepository;
 
-    public MovieSessionDto saveMovieSession(MovieSessionDto movieSessionDto) {
-        log.debug("Save movieSession: {}", movieSessionDto);
+    public MovieSession saveMovieSession(MovieSession movieSession, long movieId, long movieHallId) {
+        log.debug("Save movieSession: {}", movieSession);
 
-        MovieSession movieSession = modelMapper.map(movieSessionDto, MovieSession.class);
-
-        long movieId = movieSessionDto.getMovieId();
         Movie movie = movieRepository.findById(movieId).orElseThrow(() -> new EntityNotFoundException(movieId, Movie.class));
 
         movieSession.setMovie(movie);
 
         LocalTime endsAt = movieSession.getStartsAt().plusMinutes(movie.getDuration());
-        movieSessionDto.setEndsAt(endsAt);
         movieSession.setEndsAt(endsAt);
 
-        long movieHallId = movieSessionDto.getMovieHallId();
         MovieHall movieHall = movieHallRepository.findById(movieHallId).orElseThrow(() -> new EntityNotFoundException(movieHallId, MovieHall.class));
 
         // check if hall is available at provided time
-        boolean isMovieHallAvailable = checkMovieHallAvailability(movieHall, movieSessionDto);
+        boolean isMovieHallAvailable =
+                checkMovieHallAvailability(movieHall, movieSession.getDate(), movieSession.getStartsAt(), endsAt);
         if (!isMovieHallAvailable) {
             throw new IllegalStateException("There is another movie at hall " + movieHallId + " at that time");
         }
         movieSession.setMovieHall(movieHall);
 
-        movieSessionRepository.save(movieSession);
-        return modelMapper.map(movieSession, MovieSessionDto.class);
+        return movieSessionRepository.save(movieSession);
     }
 
     @Transactional(readOnly = true)
-    public MovieSessionDto getById(long id) {
-        MovieSession movieSession = movieSessionRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(id, MovieSession.class));
-        return modelMapper.map(movieSession, MovieSessionDto.class);
+    public MovieSession getById(long id) {
+        return movieSessionRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(id, MovieSession.class));
     }
 
     @Transactional(readOnly = true)
-    public List<MovieSessionDto> getByMovie(long movieId) {
+    public List<MovieSession> getByMovie(long movieId) {
         Movie movie = movieRepository.findById(movieId).orElseThrow(() -> new EntityNotFoundException(movieId, Movie.class));
 
-        return movieSessionRepository.findByMovie(movie).stream()
-                .map(entity -> modelMapper.map(entity, MovieSessionDto.class))
-                .collect(Collectors.toList());
+        return movieSessionRepository.findByMovie(movie);
     }
 
     @Transactional(readOnly = true)
-    public List<MovieSessionDto> getAvailableByMovie(long movieId) {
+    public List<MovieSession> getAvailableByMovie(long movieId) {
         Movie movie = movieRepository.findById(movieId).orElseThrow(() -> new EntityNotFoundException(movieId, Movie.class));
 
-        return movieSessionRepository.findAvailableByMovie(movie).stream()
-                .map(entity -> modelMapper.map(entity, MovieSessionDto.class))
-                .collect(Collectors.toList());
+        return movieSessionRepository.findAvailableByMovie(movie);
     }
 
     @Transactional(readOnly = true)
-    public List<MovieSessionDto> getByDate(LocalDate date) {
-        return movieSessionRepository.findByDate(date).stream()
-                .map(entity -> modelMapper.map(entity, MovieSessionDto.class))
-                .collect(Collectors.toList());
+    public List<MovieSession> getByDate(LocalDate date) {
+        return movieSessionRepository.findByDate(date);
     }
 
     @Transactional(readOnly = true)
-    public List<MovieSessionDto> getAvailableByDate(LocalDate date) {
-        return movieSessionRepository.findAvailableByDate(date).stream()
-                .map(entity -> modelMapper.map(entity, MovieSessionDto.class))
-                .collect(Collectors.toList());
+    public List<MovieSession> getAvailableByDate(LocalDate date) {
+        return movieSessionRepository.findAvailableByDate(date);
     }
 
     @Transactional(readOnly = true)
-    public List<MovieSessionDto> getByMovieAndDate(long movieId, LocalDate date) {
+    public List<MovieSession> getByMovieAndDate(long movieId, LocalDate date) {
         Movie movie = movieRepository.findById(movieId).orElseThrow(() -> new EntityNotFoundException(movieId, Movie.class));
 
-        return movieSessionRepository.findByMovieAndDate(movie, date).stream()
-                .map(entity -> modelMapper.map(entity, MovieSessionDto.class))
-                .collect(Collectors.toList());
+        return movieSessionRepository.findByMovieAndDate(movie, date);
     }
 
     @Transactional(readOnly = true)
-    public List<MovieSessionDto> getAvailableByMovieAndDate(long movieId, LocalDate date) {
+    public List<MovieSession> getAvailableByMovieAndDate(long movieId, LocalDate date) {
         Movie movie = movieRepository.findById(movieId).orElseThrow(() -> new EntityNotFoundException(movieId, Movie.class));
 
-        return movieSessionRepository.findAvailableByMovieAndDate(movie, date).stream()
-                .map(entity -> modelMapper.map(entity, MovieSessionDto.class))
-                .collect(Collectors.toList());
+        return movieSessionRepository.findAvailableByMovieAndDate(movie, date);
     }
 
     @Transactional(readOnly = true)
-    public List<MovieSessionDto> getAllAvailable() {
-        return movieSessionRepository.findAllAvailable().stream()
-                .map(entity -> modelMapper.map(entity, MovieSessionDto.class))
-                .collect(Collectors.toList());
+    public List<MovieSession> getAllAvailable() {
+        return movieSessionRepository.findAllAvailable();
     }
 
     @Transactional(readOnly = true)
-    public List<MovieSessionDto> getAll() {
-        return movieSessionRepository.findAll().stream()
-                .map(entity -> modelMapper.map(entity, MovieSessionDto.class))
-                .collect(Collectors.toList());
+    public List<MovieSession> getAll() {
+        return movieSessionRepository.findAll();
     }
 
-    private boolean checkMovieHallAvailability(MovieHall movieHall, MovieSessionDto movieSessionDto) {
-        List<MovieSession> movieSessionList = movieSessionRepository.findByMovieHallAndDate(movieHall, movieSessionDto.getDate());
+    private boolean checkMovieHallAvailability(MovieHall movieHall, LocalDate date, LocalTime startsAt, LocalTime endsAt) {
+        List<MovieSession> movieSessionList = movieSessionRepository.findByMovieHallAndDate(movieHall, date);
 
         for (var movieSession : movieSessionList) {
-            if (checkMovieBeginsDuringAnotherSession(movieSessionDto, movieSession) ||
-                    checkMovieEndsDuringAnotherSession(movieSessionDto, movieSession)) {
+            if (checkStartsDuringAnotherSession(startsAt, movieSession) || checkEndsDuringAnotherSession(endsAt, movieSession)) {
                 return false;
             }
         }
@@ -139,17 +113,11 @@ public class MovieSessionService {
         return true;
     }
 
-    private boolean checkMovieBeginsDuringAnotherSession(MovieSessionDto movieSessionDto, MovieSession movieSession) {
-        return movieSessionDto.getStartsAt().isAfter(movieSession.getStartsAt()) &&
-                movieSessionDto.getStartsAt().isBefore(movieSession.getEndsAt()) ||
-                movieSessionDto.getStartsAt().equals(movieSession.getStartsAt()) ||
-                movieSessionDto.getStartsAt().equals(movieSession.getEndsAt());
+    private boolean checkStartsDuringAnotherSession(LocalTime startsAt, MovieSession movieSession) {
+        return !startsAt.isBefore(movieSession.getStartsAt()) && !startsAt.isAfter(movieSession.getEndsAt());
     }
 
-    private boolean checkMovieEndsDuringAnotherSession(MovieSessionDto movieSessionDto, MovieSession movieSession) {
-        return movieSessionDto.getEndsAt().isAfter(movieSession.getStartsAt()) &&
-                movieSessionDto.getEndsAt().isBefore(movieSession.getEndsAt()) ||
-                movieSessionDto.getEndsAt().equals(movieSession.getStartsAt()) ||
-                movieSessionDto.getStartsAt().equals(movieSession.getEndsAt());
+    private boolean checkEndsDuringAnotherSession(LocalTime endsAt, MovieSession movieSession) {
+        return !endsAt.isBefore(movieSession.getStartsAt()) && !endsAt.isAfter(movieSession.getEndsAt());
     }
 }
--- src/main/java/com/example/cinema/web/MovieSessionController.java
+++ src/main/java/com/example/cinema/web/MovieSessionController.java
@@ -1,42 +1,59 @@
 package com.example.cinema.web;
 
 import com.example.cinema.model.dto.MovieSessionDto;
+import com.example.cinema.model.entities.movie.MovieSession;
 import com.example.cinema.service.MovieSessionService;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("api/movie-sessions")
 public class MovieSessionController {
+    private ModelMapper modelMapper;
     private MovieSessionService movieSessionService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public MovieSessionDto saveMovieSession(@Valid @RequestBody MovieSessionDto movieSessionDto) {
-        return movieSessionService.saveMovieSession(movieSessionDto);
+        MovieSession movieSession = modelMapper.map(movieSessionDto, MovieSession.class);
+        movieSession = movieSessionService.saveMovieSession(movieSession, movieSessionDto.getMovieId(), movieSessionDto.getMovieHallId());
+
+        return mapMovieSessionToMovieSessionDto(movieSession);
     }
 
     @GetMapping("{id}")
     public MovieSessionDto getById(@PathVariable Long id) {
-        return movieSessionService.getById(id);
+        MovieSession movieSession = movieSessionService.getById(id);
+
+        return mapMovieSessionToMovieSessionDto(movieSession);
     }
 
     @GetMapping
     public List<MovieSessionDto> getAllAvailable(@RequestParam(required = false) Long movieId, @RequestParam(required = false) LocalDate date) {
+        List<MovieSession> movieSessions;
+
         if (date != null && movieId != null) {
-            return movieSessionService.getAvailableByMovieAndDate(movieId, date);
+            movieSessions = movieSessionService.getAvailableByMovieAndDate(movieId, date);
         } else if (date != null) {
-            return movieSessionService.getAvailableByDate(date);
+            movieSessions = movieSessionService.getAvailableByDate(date);
         } else if (movieId != null) {
-            return movieSessionService.getAvailableByMovie(movieId);
+            movieSessions = movieSessionService.getAvailableByMovie(movieId);
         } else {
-            return movieSessionService.getAllAvailable();
+            movieSessions = movieSessionService.getAllAvailable();
         }
+
+        return movieSessions.stream().map(this::mapMovieSessionToMovieSessionDto).collect(Collectors.toList());
+    }
+
+    private MovieSessionDto mapMovieSessionToMovieSessionDto(MovieSession movieSession) {
+        return modelMapper.map(movieSession, MovieSessionDto.class);
     }
 }
