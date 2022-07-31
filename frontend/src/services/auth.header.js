1fd86e3bce4d21b5569281ef56b764957ce00dd8
Vitalii Psl <vitalii.psl.main@gmail.com>
2022-07-31 16:29:31 +0300

    Remove frontend files

--- frontend/src/services/auth.header.js
+++
@@ -1,9 +0,0 @@
-export default function authHeader() {
-    let user = JSON.parse(localStorage.getItem('user'));
-
-    if(user && user.jwt){
-        return 'Bearer ' + user.jwt;
-    } else {
-        return '';
-    }
-}
